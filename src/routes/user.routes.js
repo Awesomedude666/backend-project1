25bd8f69fcdde407735a986f0e96c533018c823b
Awesomedude666 <141739327+Awesomedude666@users.noreply.github.com>
2024-12-31 11:04:57 +0530

    `feat: updated user routes with additional endpoints and middleware`

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,17 @@
 import {Router} from 'express'
-import {registerUser,loginUser,logoutUser,refreshAccessToken, } from '../controllers/user.controllers.js';
+import {
+    registerUser,
+    loginUser,
+    logoutUser,
+    refreshAccessToken,
+    changeCurrentPassword,
+    getCurrentUser,
+    updateAccountDetails,
+    updateUserAvatar,
+    updateUserCoverImage,
+    getUserChannelProfile,
+    getWatchHistory,
+} from '../controllers/user.controllers.js';
 import {upload} from '../middlewares/multer.middleware.js'
 import {verifyJWT} from '../middlewares/auth.middleware.js'
 
@@ -31,7 +43,23 @@ router.route("/login").post(loginUser)
 //secured routes
 
 router.route("/logout").post(verifyJWT,logoutUser)   // and before logout method , we inject the middleware verifyJwt.
-router.route("refresh-token").post(refreshAccessToken) // here we dont need to inject any middleware becoz we already checked if user is logged in the process of checking the refreshtoken.
+router.route("/refresh-token").post(refreshAccessToken) // here we dont need to inject any middleware becoz we already checked if user is logged in the process of checking the refreshtoken.
+router.route("/change-password").post(verifyJWT,changeCurrentPassword)
+router.route("/current-user").get(verifyJWT,getCurrentUser)
+// we can also use post here but we are not sending any data so we use get.
+router.route("/update-account").patch(verifyJWT,updateAccountDetails) // here we use patch because we are updating the existing data.(a part only) if we use post here then it will create a new data.
+router.route("/avatar").patch(verifyJWT,upload.single("avatar"),updateUserAvatar) // as for uploading avatar we use another middleware upload also.
+router.route("/cover-image").patch(verifyJWT,upload.single("coverImage"),updateUserCoverImage)
+
+router.route("/c/:username").get(verifyJWT,getUserChannelProfile) // here we are using username as a parameter.
+router.route("/history").get(verifyJWT,getWatchHistory)
+
+
+
+
+
+
+
 
 
 // upload.fields() - returns middleware that processes multiple files associated with the given form fields
